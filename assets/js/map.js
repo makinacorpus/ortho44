d45a8573bc5ff2723957bfea1c1c77d4e7bd281c
ebrehault <ebrehault@gmail.com>
2013-05-24 17:31:13 +0200

    fix js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,18 +1,19 @@
 window.onload=function(){
-var map = L.map('map').setView([47.12, -1.40], 11);
+  var map = L.map('map').setView([47.12, -1.40], 11);
 
-L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-20v6611k/{z}/{x}/{y}.png', {
-  opacity: 0.5,
-  maxZoom: 19
-}).addTo(map);
+  L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-20v6611k/{z}/{x}/{y}.png', {
+    opacity: 0.5,
+    maxZoom: 19
+  }).addTo(map);
 
-L.tileLayer('http://{s}.tiles.cg44.makina-corpus.net/tiles/{z}/{x}/{y}.png', {
-  continuousWorld: true,  // very important
-  tms: true,
-  maxZoom: 19
-}).addTo(map);
+  L.tileLayer('http://{s}.tiles.cg44.makina-corpus.net/tiles/{z}/{x}/{y}.png', {
+    continuousWorld: true,  // very important
+    tms: true,
+    maxZoom: 19
+  }).addTo(map);
 
-L.control.scale().addTo(map);
+  L.control.scale().addTo(map);
 
-var osmGeocoder = new L.Control.OSMGeocoder();
-map.addControl(osmGeocoder);
+  var osmGeocoder = new L.Control.OSMGeocoder();
+  map.addControl(osmGeocoder);
+}
