89a2888b187053fd94a7df28483a2e3c6972d01d
ebrehault <ebrehault@gmail.com>
2013-05-27 16:19:10 +0200

    fix map extend

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,13 +1,13 @@
 var Ortho44 = {
   _callbackIndex: 0,
 
-  bindGeocode: function(form, input, map, callback) {
+  bindGeocode: function(form, input, map, bounds, callback) {
     L.DomEvent.addListener(form, 'submit', this._geocode, this);
     this._map = map;
     this._input = input;
     this._callback = callback;
     // Restrict to Loire-Atlantique
-    this._bounds = new L.LatLngBounds(new L.LatLng(46.86008, -2.55754), new L.LatLng(47.83486, -0.92346));
+    this._bounds = bounds;
   },
 
   _geocode: function (event) {
@@ -43,17 +43,25 @@ var Ortho44 = {
 };
 
 window.onload=function(){
-  var map = L.map('map').setView([47.12, -1.40], 11);
+  var max_bounds = new L.LatLngBounds(new L.LatLng(46.86008, -2.55754), new L.LatLng(47.83486, -0.92346));
+  var map = L.map('map',
+      {
+        'maxBounds': max_bounds,
+      }
+    ).setView([47.12, -1.40], 11);
+  map.attributionControl.setPrefix('Réalisé par <a href="http://www.makina-corpus.com">Makina Corpus</a>');
 
   var streets = L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-20v6611k/{z}/{x}/{y}.png', {
     opacity: 0.5,
-    maxZoom: 18
+    maxZoom: 18,
+    attribution: "OpenStreetMap"
   }); //.addTo(map);
 
   var ortho2012 = L.tileLayer('http://{s}.tiles.cg44.makina-corpus.net/ortho2012/{z}/{x}/{y}.png', {
     continuousWorld: true,  // very important
     tms: true,
-    maxZoom: 18
+    maxZoom: 18,
+    attribution: "Source: Département de Loire-Atlantique"
   }).addTo(map);
 
   var baseMaps = {
@@ -66,7 +74,12 @@ window.onload=function(){
 
   L.control.scale().addTo(map);
 
-  Ortho44.bindGeocode(document.getElementById('search-address'), document.getElementById("search-input"), map, function (results) {
+  Ortho44.bindGeocode(
+    document.getElementById('search-address'),
+    document.getElementById("search-input"),
+    map,
+    max_bounds,
+    function (results) {
       console.log(results);
       if(results.length > 0) {
         var bbox = results[0].boundingbox,
